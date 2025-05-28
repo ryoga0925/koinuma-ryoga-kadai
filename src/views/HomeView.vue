2e9445ddda4cab72aef9f4f5bf26a1cfeee563e9
ryoga0925 <rkoinuma@dl-techx-xedge.net>
2025-05-28 17:44:25 +0900

    viewtify仕様に変更

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <ProductList /> <!--Productlistのコンポーネントを描画-->
-  </div>
+  <hello-world />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import ProductList from '@/components/ProductList.vue' //ProductList.vueをインポート
+  import HelloWorld from '../components/HelloWorld'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-    ProductList //ProductListを追加
+  export default {
+    name: 'HomeView',
+
+    components: {
+      HelloWorld,
+    },
   }
-}
 </script>
